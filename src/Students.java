1f0a0408e87072c6d68b235dbcba8040ba002c87
nursultan <alymbekovnursultan232@gmail.com>
2024-12-10 11:49:05 +0600

    updated main

---
+++ src/Students.java
@@ -0,0 +1,11 @@
+public class Students {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String email;
+
+    public Students() {
+    }
+
+}
